2377748945bb4aa6f302f679d7d9d5f2ce145894
npodonnell <6617714+npodonnell@users.noreply.github.com>
2021-05-22 14:55:56 +0100

    epoll

--- counter.c
+++ counter.c
@@ -1,7 +1,10 @@
+#include <sys/epoll.h>
+#include <sys/socket.h>
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int shutting_down = 0;
 
@@ -36,8 +39,67 @@ int setup_signal_handlers() {
     return 0;
 }
 
-void cleanup() {
-    printf("Cleaning up...\n");
+int ep_init(int nsocks, ...) {
+    int efd = epoll_create1(0);
+    
+    va_list valist;
+    va_start(valist, nsocks);
+    
+    for (int i = 0;  i < nsocks; i++) {
+        int fd = va_arg(valist, int);
+        printf("Adding %d to epoll\n", fd);
+        
+        struct epoll_event event;
+        
+        event.data.u64 = (uint64_t)0;
+        event.data.fd = fd;
+        event.events = EPOLLET | EPOLLIN | EPOLLOUT | EPOLLHUP;
+        
+        if (epoll_ctl(efd, EPOLL_CTL_ADD, fd, &event) == -1) {
+            fprintf(stderr, "Failure when adding %d to epoll instance", fd);
+            close(efd);
+            return -1;
+        }
+    }
+    
+    va_end(valist);
+    return efd;
+}
+
+void ep_cleanup(int efd, int nsocks, ...) {
+    va_list valist;
+    va_start(valist, nsocks);
+    
+    for (int i = 0;  i < nsocks; i++) {
+        int fd = va_arg(valist, int);
+        printf("Removing %d from epoll\n", fd);
+    
+        struct epoll_event dummy;
+    
+        if (epoll_ctl(efd, EPOLL_CTL_DEL, fd, &dummy) == -1) {
+            fprintf(stderr, "Failure when removing %d from epoll instance", fd);
+        }
+    }
+
+    va_end(valist);
+    
+    close(efd);
+}
+
+void count() {
+    int sock_tcp, sock_udp, efd;
+    
+    sock_tcp = socket(AF_INET, SOCK_STREAM, 0);
+    sock_udp = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    if ((efd = ep_init(2, sock_tcp, sock_udp)) != -1) {
+        while (!shutting_down) {
+            printf("wait\n");
+            sleep(5);
+        }
+    }
+    
+    ep_cleanup(efd, 2, sock_tcp, sock_udp);
 }
 
 int main(int argc, char** argv) {
@@ -48,12 +110,7 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
     
-    while (!shutting_down) {
-        printf("wait\n");
-        sleep(5);
-    }
-    
-    cleanup();
+    count();
     
     return EXIT_SUCCESS;
 }
